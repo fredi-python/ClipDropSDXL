a60dc635ddd92ab7fd41ffb8cb7ef51601bde074
fredi-python <frederik.boessner@gmx.de>
2023-07-04 00:21:41 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='ClipDropSDXL',
+    version='0.0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'ClipDropSDXL = ClipDropSDXL.main:main'
+        ]
+    },
+    install_requires=[
+        "selenium"
+        "argparse"
+    ],
+    author='Your Name',
+    author_email='fredi@mt-oneblock.net',
+    description='Selenium Wrapper for ClipDrop: Unlocking High-Resolution Text-to-Image Creation with Stable Diffusion XL (SDXL)',
+    url='https://github.com/fredi-python/ClipDropSDXL',
+)
