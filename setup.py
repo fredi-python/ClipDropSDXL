8ad9d627e4aa311d6b681f0570836406742b2844
fredi-python <frederik.boessner@gmx.de>
2023-08-12 01:59:07 +0200

    Fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ClipDropSDXL',
-    version='0.0.3',
+    version='0.0.1.3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points={
