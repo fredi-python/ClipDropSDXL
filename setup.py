273a85aa3f53414e2fb5b47634ff7c8915d64d52
fredi-python <frederik.boessner@gmx.de>
2023-08-12 02:51:58 +0200

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ClipDropSDXL',
-    version='0.0.1.3',
+    version='0.0.1.4',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points={
